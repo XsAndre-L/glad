44052dae156b70ee36b7f61b737247675d6925a4
xsandre-l <ahloubser12@gmail.com>
2025-12-20 06:08:23 +0200

    .

--- .codeframe/package.ts
+++ .codeframe/package.ts
@@ -1,54 +1,47 @@
-import { BuildType, OUTPUT_DIR } from "../../../src/types/package-config.ts";
-import { runPackageAction } from "../../../src/commands/packages.ts";
+import {
+  BuildType,
+  CPP_OUTPUT_DIR,
+  runPackageAction,
+  CMAKE_TOOLS,
+  getHostSysrootPath,
+  SYSROOT,
+  BuildConfiguration,
+  LibraryInfo,
+} from "../../../../src/providers/package.provider.ts";
 
-import { resolve, join } from "node:path";
+import { join } from "node:path";
 import { argv } from "node:process";
 
+export const info: LibraryInfo = {
+  name: "glad",
+  outDir: "build",
+  version: "4.6.0",
+};
+
 export const build = (cwd: string = process.cwd()): BuildType => {
-  const toolchain = resolve(cwd, "../../toolchains/llvm-mingw");
-  const CLANG = join(toolchain, "bin/clang.exe").replace(/\\/g, "/");
-  const CLANGXX = join(toolchain, "bin/clang++.exe").replace(/\\/g, "/");
-  const WINDRES = join(toolchain, "bin/llvm-windres.exe").replace(/\\/g, "/");
+  const HOST_SYSROOT = getHostSysrootPath();
+  const CLANG = join(HOST_SYSROOT, "bin/clang.exe").replace(/\\/g, "/");
+  const CLANGXX = join(HOST_SYSROOT, "bin/clang++.exe").replace(/\\/g, "/");
+  const WINDRES = join(HOST_SYSROOT, "bin/llvm-windres.exe").replace(
+    /\\/g,
+    "/"
+  );
   const AARCH64_WINDRES = join(
-    toolchain,
+    HOST_SYSROOT,
     "bin/aarch64-w64-mingw32-windres.exe"
   ).replace(/\\/g, "/");
 
   return {
-    type: "architectures",
+    type: "compilation",
     windows_x86_64: {
-      configStep: `cmake -S ./cmake -B build/windows/x86_64 -G Ninja \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_SHARED_LIBS=OFF \
-      -DCMAKE_C_COMPILER=${CLANG} \
-      -DCMAKE_CXX_COMPILER=${CLANGXX} \
-      -DCMAKE_RC_COMPILER=${WINDRES} \
-      -DCMAKE_C_COMPILER_TARGET=x86_64-w64-windows-gnu \
-      -DCMAKE_CXX_COMPILER_TARGET=x86_64-w64-windows-gnu \
-      -DCMAKE_SYSTEM_NAME=Windows \
-      -DCMAKE_SYSTEM_PROCESSOR=x86_64 \
-      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/windows/x86_64/glad
-    `,
-
-      buildStep: `cmake --build build/windows/x86_64 -j`,
-      installStep: `cmake --install build/windows/x86_64`,
+      configStep: `uv run glad --api gl:core=4.6 --out-path ${info.outDir} c`,
+      buildStep: ``,
+      installStep: ``,
     },
     windows_aarch64: {
-      configStep: `cmake -S ./cmake -B build/windows/aarch64 -G Ninja \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_SHARED_LIBS=OFF \
-      -DCMAKE_C_COMPILER=${CLANG} \
-      -DCMAKE_CXX_COMPILER=${CLANGXX} \
-      -DCMAKE_RC_COMPILER=${AARCH64_WINDRES} \
-      -DCMAKE_RC_FLAGS=--target=aarch64-w64-mingw32 \
-      -DCMAKE_C_COMPILER_TARGET=aarch64-w64-windows-gnu \
-      -DCMAKE_CXX_COMPILER_TARGET=aarch64-w64-windows-gnu \
-      -DCMAKE_SYSTEM_NAME=Windows \
-      -DCMAKE_SYSTEM_PROCESSOR=aarch64 \
-      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/windows/aarch64/glad
-      `,
-      buildStep: `cmake --build build/windows/aarch64 -j`,
-      installStep: `cmake --install build/windows/aarch64`,
+      configStep: ``,
+      buildStep: ``,
+      installStep: ``,
     },
     linux_x86_64: {
       configStep: ``,
@@ -66,4 +59,9 @@ export const build = (cwd: string = process.cwd()): BuildType => {
 const args = argv.slice(2);
 const [action = "help"] = args;
 
-await runPackageAction(action, process.cwd(), build());
+const buildConfig: BuildConfiguration = {
+  info,
+  build: build(),
+};
+
+await runPackageAction(action, process.cwd(), buildConfig);
